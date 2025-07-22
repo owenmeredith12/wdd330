a8920bf550ffdefbad735de7bb85111d693706f9
owenmeredith12 <owen.meredith1516@gmail.com>
2025-07-21 22:27:00 -0600

    more changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        product_listing: resolve(__dirname, "src/product_listing/index.html"),
+        main: resolve(__dirname, 'src/index.html'),
+        cart: resolve(__dirname, 'src/cart/index.html'),
+        checkout: resolve(__dirname, 'src/checkout/index.html'),
+        product: resolve(__dirname, 'src/product_pages/index.html'),
+        product_listing: resolve(__dirname, 'src/product_listing/index.html'),
       },
     },
   },
-  base: "./",
+  base: './',
 });
