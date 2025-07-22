7494ddda60c05faebf570cc7a587490fcad05b41
owenmeredith12 <owen.meredith1516@gmail.com>
2025-07-21 19:39:57 -0600

    more live site changes

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,18 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
-
   build: {
     outDir: "../dist",
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        product_listing: resolve(__dirname, "src/product_listing/index.html"),
+        main: "index.html",
+        cart: "cart/index.html",
+        checkout: "checkout/index.html",
+        product: "product_pages/index.html",
+        product_listing: "product_listing/index.html",
       },
     },
   },
+  base: "./",
 });
