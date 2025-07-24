ca370c1af952110baccb5f85df5b5f9f418386a3
SantiagoHrd <santiagohrd1@gmail.com>
2025-07-23 20:25:51 -0500

    Add product-list Sort

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -7,6 +7,30 @@ loadHeaderFooter();
 const category = getParam('category');
 const dataSource = new ProductData();
 const element = document.querySelector('.product-list');
-const listing = new ProductList(category, dataSource, element);
+const productlist = new ProductList(category, dataSource, element);
 
-listing.init();
+let originalList = [];
+
+async function sortedListnit() {
+  const list = await dataSource.getData(category);
+  originalList = list;
+  productlist.renderList(originalList);
+  document.querySelector('.title').textContent = category;
+}
+
+sortedListnit()
+
+productlist.init();
+
+document.getElementById('sort').addEventListener('change', (e) => {
+    let sortedList = [...originalList]; // cópia da lista original
+    const value = e.target.value;
+  
+    if (value === 'name') {
+      sortedList.sort((a, b) => a.NameWithoutBrand.localeCompare(b.NameWithoutBrand));
+    } else if (value === 'price') {
+      sortedList.sort((a, b) => a.FinalPrice - b.FinalPrice);
+    }
+  
+    productlist.renderList(sortedList);
+});
